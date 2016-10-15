74e14a08bb4554eb3bab14d630d07d9de4acfdf7
lotbs <LookOnTheBrightSide@users.noreply.github.com>
2016-10-15 08:00:54 +0100

    Thanks for the examples! (#1)
    * :curly_loop: :thought_balloon:
    
    * :wrench:

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Sample Scripts
 This repository contains sample scripts to show certain concepts for UCD COMP30640. The scripts may be in a variety of programming and scripting languages, usually python or bash
 
-Obviously, this repository does not contain answers to any practicals, but contains some examples that you can learn from and test on your own computer. 
+Obviously, this repository does not contain answers to any practicals, but contains some examples that you can learn from and test on your own computer.
 
 
 ## The index
 ### make_directories_loop
-Contains files that demonstrate how to use loops in bash. 
+Contains files that demonstrate how to use loops in bash.
 
 ```make_lab_directories.sh``` uses a for loop and a predefined list
 
@@ -18,3 +18,7 @@ Contains files that demonstrate how to use loops in bash.
 Recursion can be complicated to get your head around. This directory contains samples used to demonstrate this method of programming
 
 ```recursive_countdown.py```, and the python3 alternative, counts down from a specific value, passed as a command line argument. It uses prints to trace the running of the program.
+
+A video that might help.
+
+[![Alt text](https://img.youtube.com/vi/Mv9NEXX1VHc/0.jpg)](https://www.youtube.com/watch?v=Mv9NEXX1VHc)
--- recursion/recursive_countdown_py3.py
+++ recursion/recursive_countdown_py3.py
@@ -1,20 +1,27 @@
 import sys
 
 # this script uses python 3. For python , see recursive_countdown.py
-# Run this script from the command line using "python recursive_countdown.py #" where # is replaced with an integer number of your choosing
+# Run this script from the command line using "python recursive_countdown.py #" or "python3 recursive_countdown.py #" where # is replaced with an integer number of your choosing
 # Note that you will need to cd into the same directory as this file. You can also call this script using "python /path/to/this/file.py #"
 # Once you understand this script you should be able to write up to step 3 of the In an Indian File
 
-
+# a function that takes a number (integer) as a parameter // 10
 def chain(n):
-	print("n is " + str(n))
+	# displays the value of the argument(number) // 10
+	print("n is {}".format(n))
+	# the following is true as long as the value of n is greater or equal to one // if 10 >= 1
 	if n >= 1:
+		# variable k subracts 1 from n // k = 9
 		k = n - 1;
-		print("there are " + str(k) + " more functions to execute")
+		# there are 9 more functions to execute
+		print("there are {} more functions to execute".format(k))
+		# the function calls itself with the previous number as an argument // chain(9)
+		# the magic happens here, the function will keep calling itself until line number 13 fails
 		chain(k)
+	# Once that fails, the following is printed
 	else:
 		print("No more chain to create. End of sequence")
-	print("Finishing function n = " + str(n))
+	print("Finishing function n = {}".format(n))
 
 
 
